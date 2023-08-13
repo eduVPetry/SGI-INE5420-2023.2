663b10ab6aba643efd671740ee41d73b53081f04
eduVPetry <eduvp97@gmail.com>
2023-08-13 16:16:15 -0300

    Add: dialog for adding new graphical objects
    Add button now opens a dialog for adding new graphical objects.
    Code of main window has been refactored to its own file.

---
+++ dialog.py
@@ -0,0 +1,164 @@
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Dialog(QtWidgets.QDialog):
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.setObjectName("Dialog")
+        self.setWindowTitle("Add Object")
+        self.resize(640, 480)
+        size_policy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        size_policy.setHorizontalStretch(0)
+        size_policy.setVerticalStretch(0)
+        size_policy.setHeightForWidth(self.sizePolicy().hasHeightForWidth())
+        self.setSizePolicy(size_policy)
+
+        # Tabs
+        self.tab_widget = QtWidgets.QTabWidget(self)
+        self.tab_widget.setGeometry(QtCore.QRect(30, 80, 571, 331))
+        self.tab_widget.setObjectName("tabWidget")
+
+        self.tab = QtWidgets.QWidget()
+        self.tab_2 = QtWidgets.QWidget()
+        self.tab_3 = QtWidgets.QWidget()
+
+        self.tab.setObjectName("tab")
+        self.tab_2.setObjectName("tab_2")
+        self.tab_3.setObjectName("tab_3")
+
+        self.tab_widget.addTab(self.tab, "")
+        self.tab_widget.addTab(self.tab_2, "")
+        self.tab_widget.addTab(self.tab_3, "")
+
+        self.tab_widget.setTabText(0, "Point")
+        self.tab_widget.setTabText(1, "Line")
+        self.tab_widget.setTabText(2, "Wireframe")
+
+        self.tab_widget.setCurrentIndex(0)
+
+        # Ok and Cancel Buttons
+        self.button_box = QtWidgets.QDialogButtonBox(self)
+        self.button_box.setGeometry(QtCore.QRect(10, 440, 621, 32))
+        self.button_box.setOrientation(QtCore.Qt.Horizontal)
+        self.button_box.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel|QtWidgets.QDialogButtonBox.Ok)
+        self.button_box.setObjectName("buttonBox")
+        self.button_box.accepted.connect(self.ok_callback)
+        self.button_box.rejected.connect(self.cancel_callback)
+
+        # Fonts for the labels
+        font = QtGui.QFont()
+        font.setPointSize(10)
+
+        font_bold = QtGui.QFont()
+        font_bold.setPointSize(10)
+        font_bold.setBold(True)
+
+        # Labels
+        self.label = QtWidgets.QLabel(self)
+        self.label.setGeometry(QtCore.QRect(30, 10, 57, 15))
+        self.label.setFont(font_bold)
+        self.label.setText("Name")
+        self.label.setObjectName("label")
+
+        self.label_2 = QtWidgets.QLabel(self.tab)
+        self.label_2.setGeometry(QtCore.QRect(60, 90, 16, 16))
+        self.label_2.setFont(font)
+        self.label_2.setText("x")
+        self.label_2.setObjectName("label_2")
+
+        self.label_3 = QtWidgets.QLabel(self.tab)
+        self.label_3.setGeometry(QtCore.QRect(230, 90, 57, 15))
+        self.label_3.setFont(font)
+        self.label_3.setText("y")
+        self.label_3.setObjectName("label_3")
+
+        self.label_4 = QtWidgets.QLabel(self.tab_2)
+        self.label_4.setGeometry(QtCore.QRect(30, 10, 91, 31))
+        self.label_4.setFont(font_bold)
+        self.label_4.setText("Start Point")
+        self.label_4.setObjectName("label_4")
+
+        self.label_5 = QtWidgets.QLabel(self.tab_2)
+        self.label_5.setGeometry(QtCore.QRect(30, 140, 71, 16))
+        self.label_5.setFont(font_bold)
+        self.label_5.setText("End Point")
+        self.label_5.setObjectName("label_5")
+
+        self.label_6 = QtWidgets.QLabel(self.tab_2)
+        self.label_6.setGeometry(QtCore.QRect(220, 80, 57, 15))
+        self.label_6.setFont(font)
+        self.label_6.setText("y1")
+        self.label_6.setObjectName("label_6")
+
+        self.label_7 = QtWidgets.QLabel(self.tab_2)
+        self.label_7.setGeometry(QtCore.QRect(50, 80, 16, 16))
+        self.label_7.setFont(font)
+        self.label_7.setText("x1")
+        self.label_7.setObjectName("label_7")
+
+        self.label_8 = QtWidgets.QLabel(self.tab_2)
+        self.label_8.setGeometry(QtCore.QRect(220, 200, 57, 15))
+        self.label_8.setFont(font)
+        self.label_8.setText("y2")
+        self.label_8.setObjectName("label_8")
+
+        self.label_9 = QtWidgets.QLabel(self.tab_2)
+        self.label_9.setGeometry(QtCore.QRect(50, 200, 16, 16))
+        self.label_9.setFont(font)
+        self.label_9.setText("x2")
+        self.label_9.setObjectName("label_9")
+
+        self.label_10 = QtWidgets.QLabel(self.tab)
+        self.label_10.setGeometry(QtCore.QRect(40, 30, 141, 16))
+        self.label_10.setFont(font_bold)
+        self.label_10.setText("Point Coordinates")
+        self.label_10.setObjectName("label_10")
+
+        # Plain text inputs
+        self.plain_text_edit = QtWidgets.QPlainTextEdit(self)
+        self.plain_text_edit.setGeometry(QtCore.QRect(30, 30, 191, 31))
+        self.plain_text_edit.setObjectName("plainTextEdit")
+
+        self.plain_text_edit_2 = QtWidgets.QPlainTextEdit(self.tab)
+        self.plain_text_edit_2.setGeometry(QtCore.QRect(80, 80, 111, 31))
+        self.plain_text_edit_2.setObjectName("plainTextEdit_2")
+
+        self.plain_text_edit_3 = QtWidgets.QPlainTextEdit(self.tab)
+        self.plain_text_edit_3.setGeometry(QtCore.QRect(250, 80, 111, 31))
+        self.plain_text_edit_3.setObjectName("plainTextEdit_3")
+
+        self.plain_text_edit_4 = QtWidgets.QPlainTextEdit(self.tab_2)
+        self.plain_text_edit_4.setGeometry(QtCore.QRect(240, 70, 111, 31))
+        self.plain_text_edit_4.setObjectName("plainTextEdit_4")
+
+        self.plain_text_edit_5 = QtWidgets.QPlainTextEdit(self.tab_2)
+        self.plain_text_edit_5.setGeometry(QtCore.QRect(70, 70, 111, 31))
+        self.plain_text_edit_5.setObjectName("plainTextEdit_5")
+
+        self.plain_text_edit_6 = QtWidgets.QPlainTextEdit(self.tab_2)
+        self.plain_text_edit_6.setGeometry(QtCore.QRect(240, 190, 111, 31))
+        self.plain_text_edit_6.setObjectName("plainTextEdit_6")
+
+        self.plain_text_edit_7 = QtWidgets.QPlainTextEdit(self.tab_2)
+        self.plain_text_edit_7.setGeometry(QtCore.QRect(70, 190, 111, 31))
+        self.plain_text_edit_7.setObjectName("plainTextEdit_7")
+
+        QtCore.QMetaObject.connectSlotsByName(self)
+
+    def ok_callback(self):
+        print("OK")
+        self.close()
+    
+    def cancel_callback(self):
+        print("CANCEL")
+        self.close()
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    dialog = Dialog()
+    dialog.show()
+    sys.exit(app.exec())
---
+++ main_window.py
@@ -0,0 +1,233 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+from dialog import Dialog
+
+
+class MainWindow(QtWidgets.QMainWindow):
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.setObjectName("mainWindow")
+        self.setWindowTitle("Sistema Gráfico Interativo")
+        self.resize(1000, 600)
+
+        self.centralWidget = QtWidgets.QWidget(self)
+        self.centralWidget.setObjectName("centralWidget")
+
+        # Font for the labels
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        font.setBold(True)
+
+        # Widget for the layout in the left side of the UI
+        self.left_widget = QtWidgets.QWidget(self.centralWidget)
+        self.left_widget.setGeometry(QtCore.QRect(11, 10, 256, 431))
+        self.left_widget.setObjectName("widgetLeft")
+
+        # Vertical layout in the left side of the UI
+        self.vertical_layout = QtWidgets.QVBoxLayout(self.left_widget)
+        self.vertical_layout.setContentsMargins(0, 0, 0, 0)
+        self.vertical_layout.setObjectName("verticalLayout")
+
+        # Objects table label
+        self.object_table_label = QtWidgets.QLabel(self.left_widget)
+        self.object_table_label.setFont(font)
+        self.object_table_label.setScaledContents(False)
+        self.object_table_label.setObjectName("objTableLabel")
+        self.object_table_label.setText("Objects")
+
+        self.vertical_layout.addWidget(self.object_table_label)
+
+        # Objects table (type, name)
+        self.table_widget = QtWidgets.QTableWidget(self.left_widget)
+        self.table_widget.setEnabled(True)
+        self.table_widget.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        self.table_widget.setDragEnabled(True)
+        self.table_widget.setTextElideMode(QtCore.Qt.ElideLeft)
+        self.table_widget.setObjectName("tableWidget")
+        self.table_widget.setColumnCount(2)
+        self.table_widget.setRowCount(0)
+    
+        item = QtWidgets.QTableWidgetItem()
+        item.setText("Type")
+        self.table_widget.setHorizontalHeaderItem(0, item)
+
+        item = QtWidgets.QTableWidgetItem()
+        item.setText("Name")
+        self.table_widget.setHorizontalHeaderItem(1, item)
+
+        self.table_widget.horizontalHeader().setCascadingSectionResizes(True)
+
+        self.vertical_layout.addWidget(self.table_widget)
+
+        # Add, remove and clear buttons in a horizontal layout
+        self.horizontal_layout = QtWidgets.QHBoxLayout()
+        self.horizontal_layout.setObjectName("horizontalLayout")
+        
+        self.add_button = QtWidgets.QPushButton(self.left_widget)
+        self.add_button.setObjectName("addButton")
+        self.add_button.setText("Add")
+        self.add_button.clicked.connect(self.add_button_clicked)
+        self.horizontal_layout.addWidget(self.add_button)
+
+        self.remove_button = QtWidgets.QPushButton(self.left_widget)
+        self.remove_button.setObjectName("removeButton")
+        self.remove_button.setText("Remove")
+        self.remove_button.clicked.connect(self.remove_button_clicked)
+        self.horizontal_layout.addWidget(self.remove_button)
+
+        self.clear_button = QtWidgets.QPushButton(self.left_widget)
+        self.clear_button.setObjectName("clearButton")
+        self.clear_button.setText("Clear")
+        self.clear_button.clicked.connect(self.clear_button_clicked)
+        self.horizontal_layout.addWidget(self.clear_button)
+
+        self.vertical_layout.addLayout(self.horizontal_layout)
+
+        # Espaço entre os layouts
+        self.vertical_layout.addItem(QtWidgets.QSpacerItem(20, 20, QtWidgets.QSizePolicy.Fixed))
+
+        # Control label
+        self.control_label = QtWidgets.QLabel(self.left_widget)
+        self.control_label.setFont(font)
+        self.control_label.setObjectName("controlLabel")
+        self.control_label.setText("Control")
+
+        self.vertical_layout.addWidget(self.control_label)
+
+        # Window control buttons in a grid layout:
+        #   Zoom in,   up, zoom out
+        #      left,     , right
+        # rot. left, down, rot. right
+
+        self.grid_layout = QtWidgets.QGridLayout()
+        self.grid_layout.setObjectName("gridLayout")
+        
+        self.zoom_out_button = QtWidgets.QPushButton(self.left_widget)
+        self.zoom_out_button.setObjectName("zoomOutButton")
+        self.zoom_out_button.setText("-")
+        self.zoom_out_button.clicked.connect(self.zoom_out_button_clicked)
+        self.grid_layout.addWidget(self.zoom_out_button, 0, 0, 1, 1)
+
+        self.up_button = QtWidgets.QPushButton(self.left_widget)
+        self.up_button.setObjectName("upButton")
+        self.up_button.setText("up")
+        self.up_button.clicked.connect(self.up_button_clicked)
+        self.grid_layout.addWidget(self.up_button, 0, 1, 1, 1)
+
+        self.zoom_in_button = QtWidgets.QPushButton(self.left_widget)
+        self.zoom_in_button.setObjectName("zoomInButton")
+        self.zoom_in_button.setText("+")
+        self.zoom_in_button.clicked.connect(self.zoom_in_button_clicked)
+        self.grid_layout.addWidget(self.zoom_in_button, 0, 2, 1, 1)
+
+        self.left_button = QtWidgets.QPushButton(self.left_widget)
+        self.left_button.setObjectName("leftButton")
+        self.left_button.setText("left")
+        self.left_button.clicked.connect(self.left_button_clicked)
+        self.grid_layout.addWidget(self.left_button, 1, 0, 1, 1)
+
+        self.right_button = QtWidgets.QPushButton(self.left_widget)
+        self.right_button.setObjectName("rightButton")
+        self.right_button.setText("right")
+        self.right_button.clicked.connect(self.right_button_clied)
+        self.grid_layout.addWidget(self.right_button, 1, 2, 1, 1)
+
+        self.rotate_left_button = QtWidgets.QPushButton(self.left_widget)
+        self.rotate_left_button.setObjectName("rotateLeftButton")
+        self.rotate_left_button.setText("rot. left")
+        self.rotate_left_button.clicked.connect(self.rotate_left_button_clicked)
+        self.grid_layout.addWidget(self.rotate_left_button, 2, 0, 1, 1)
+
+        self.down_button = QtWidgets.QPushButton(self.left_widget)
+        self.down_button.setObjectName("downButton")
+        self.down_button.setText("down")
+        self.down_button.clicked.connect(self.down_button_clicked)
+        self.grid_layout.addWidget(self.down_button, 2, 1, 1, 1)
+
+        self.rotate_right_button = QtWidgets.QPushButton(self.left_widget)
+        self.rotate_right_button.setObjectName("rotateRightButton")
+        self.rotate_right_button.setText("rot. right")
+        self.rotate_right_button.clicked.connect(self.rotate_right_button_clicked)
+        self.grid_layout.addWidget(self.rotate_right_button, 2, 2, 1, 1)
+
+        self.vertical_layout.addLayout(self.grid_layout)
+
+        # Widget for the layout in the right side of the UI
+        self.right_widget = QtWidgets.QWidget(self.centralWidget)
+        self.right_widget.setGeometry(QtCore.QRect(321, 15, 661, 571))
+        self.right_widget.setObjectName("rightWidget")
+
+        # Vertical layout in the right side of the UI
+        self.vertical_layout_2 = QtWidgets.QVBoxLayout(self.right_widget)
+        self.vertical_layout_2.setContentsMargins(0, 0, 0, 0)
+        self.vertical_layout_2.setObjectName("verticalLayout_2")
+
+        # Viewport label
+        self.viewport_label = QtWidgets.QLabel(self.right_widget)
+        self.viewport_label.setFont(font)
+        self.viewport_label.setObjectName("viewportLabel")
+        self.viewport_label.setText("Viewport")
+        self.vertical_layout_2.addWidget(self.viewport_label)
+
+        # Viewport widget
+        self.viewport_widget = QtWidgets.QGraphicsView(self.right_widget)
+        self.viewport_widget.setFixedWidth(660)
+        self.viewport_widget.setFixedHeight(340)
+        size_policy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        # sizePolicy.setHorizontalStretch(0)
+        # sizePolicy.setVerticalStretch(0)
+        # sizePolicy.setHeightForWidth(self.graphicsView.sizePolicy().hasHeightForWidth())
+        self.viewport_widget.setSizePolicy(size_policy)
+        self.viewport_widget.setObjectName("viewportWidget")
+        self.vertical_layout_2.addWidget(self.viewport_widget)
+
+        # Text console for debugging
+        self.text_console = QtWidgets.QTextBrowser(self.right_widget)
+        self.text_console.setObjectName("textConsole")
+        self.vertical_layout_2.addWidget(self.text_console)
+
+        # Top-level horizontal layout
+        self.horizontal_layout_2 = QtWidgets.QHBoxLayout(self.centralWidget)
+        self.horizontal_layout_2.setObjectName("horizontalLayout_2")
+        self.horizontal_layout_2.addWidget(self.left_widget)
+        self.horizontal_layout_2.addWidget(self.right_widget)
+
+        self.setCentralWidget(self.centralWidget)
+
+        QtCore.QMetaObject.connectSlotsByName(self)
+
+    def add_button_clicked(self):
+        dialog = Dialog()
+        dialog.exec()
+
+    def remove_button_clicked(self):
+        ...
+
+    def clear_button_clicked(self):
+        ...
+
+    def up_button_clicked(self):
+        ...
+
+    def down_button_clicked(self):
+        ...
+
+    def left_button_clicked(self):
+        ...
+
+    def right_button_clied(self):
+        ...
+
+    def zoom_out_button_clicked(self):
+        ...
+
+    def zoom_in_button_clicked(self):
+        ...
+
+    def rotate_left_button_clicked(self):
+        ...
+
+    def rotate_right_button_clicked(self):
+        ...
