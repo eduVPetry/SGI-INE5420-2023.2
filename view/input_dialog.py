c0a377e168bb40a877386f18431c0385909513c8
eduVPetry <eduvp97@gmail.com>
2023-08-21 00:57:38 -0300

    Working: wireframe .csv files and drawing correctly

--- view/input_dialog.py
+++ view/input_dialog.py
@@ -1,4 +1,9 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import Qt, QPointF, QRect, QMetaObject
+from PyQt5.QtGui import QDoubleValidator, QFont
+from PyQt5.QtWidgets import (
+    QDialog, QDialogButtonBox, QFileDialog, QLineEdit, QPushButton,
+    QTabWidget, QTextEdit, QVBoxLayout, QWidget
+)
 
 from model.point import Point
 from model.line import Line
@@ -6,7 +11,7 @@ from model.wireframe import Wireframe
 from view.label import Label
 
 
-class InputDialog(QtWidgets.QDialog):
+class InputDialog(QDialog):
 
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -17,22 +22,22 @@ class InputDialog(QtWidgets.QDialog):
         self.resize(640, 480)
 
         # Tabs
-        self.tab_widget = QtWidgets.QTabWidget(self)
-        self.tab_widget.setGeometry(QtCore.QRect(30, 80, 571, 331))
+        self.tab_widget = QTabWidget(self)
+        self.tab_widget.setGeometry(QRect(30, 80, 571, 331))
 
-        self.point_tab = QtWidgets.QWidget()
-        self.line_tab = QtWidgets.QWidget()
-        self.wireframe_tab = QtWidgets.QWidget()
+        self.point_tab = QWidget()
+        self.line_tab = QWidget()
+        self.wireframe_tab = QWidget()
 
         self.tab_widget.addTab(self.point_tab, "Point")
         self.tab_widget.addTab(self.line_tab, "Line")
         self.tab_widget.addTab(self.wireframe_tab, "Wireframe")
 
         # Fonts for the labels
-        font = QtGui.QFont()
+        font = QFont()
         font.setPointSize(12)
 
-        font_bold = QtGui.QFont()
+        font_bold = QFont()
         font_bold.setPointSize(12)
         font_bold.setBold(True)
 
@@ -48,36 +53,36 @@ class InputDialog(QtWidgets.QDialog):
         self.line_x2_label = Label("x2", font, self.line_tab)
         self.line_y2_label = Label("y2", font, self.line_tab)
 
-        self.name_label.setGeometry(QtCore.QRect(30, 10, 57, 15))
-        self.point_label.setGeometry(QtCore.QRect(40, 30, 141, 16))
-        self.point_x_label.setGeometry(QtCore.QRect(60, 90, 16, 16))
-        self.point_y_label.setGeometry(QtCore.QRect(230, 90, 57, 15))
-        self.line_start_label.setGeometry(QtCore.QRect(30, 10, 180, 31))
-        self.line_end_label.setGeometry(QtCore.QRect(30, 140, 175, 16))
-        self.line_x1_label.setGeometry(QtCore.QRect(50, 80, 16, 16))
-        self.line_y1_label.setGeometry(QtCore.QRect(220, 80, 57, 15))
-        self.line_x2_label.setGeometry(QtCore.QRect(50, 200, 16, 16))
-        self.line_y2_label.setGeometry(QtCore.QRect(220, 200, 57, 15))
+        self.name_label.setGeometry(QRect(30, 10, 57, 15))
+        self.point_label.setGeometry(QRect(40, 30, 141, 16))
+        self.point_x_label.setGeometry(QRect(60, 90, 16, 16))
+        self.point_y_label.setGeometry(QRect(230, 90, 57, 15))
+        self.line_start_label.setGeometry(QRect(30, 10, 180, 31))
+        self.line_end_label.setGeometry(QRect(30, 140, 175, 16))
+        self.line_x1_label.setGeometry(QRect(50, 80, 16, 16))
+        self.line_y1_label.setGeometry(QRect(220, 80, 57, 15))
+        self.line_x2_label.setGeometry(QRect(50, 200, 16, 16))
+        self.line_y2_label.setGeometry(QRect(220, 200, 57, 15))
 
         # Text inputs
-        self.name_input = QtWidgets.QLineEdit(self)
-        self.point_x_input = QtWidgets.QLineEdit(self.point_tab)
-        self.point_y_input = QtWidgets.QLineEdit(self.point_tab)
-        self.line_x1_input = QtWidgets.QLineEdit(self.line_tab)
-        self.line_y1_input = QtWidgets.QLineEdit(self.line_tab)
-        self.line_x2_input = QtWidgets.QLineEdit(self.line_tab)
-        self.line_y2_input = QtWidgets.QLineEdit(self.line_tab)
+        self.name_input = QLineEdit(self)
+        self.point_x_input = QLineEdit(self.point_tab)
+        self.point_y_input = QLineEdit(self.point_tab)
+        self.line_x1_input = QLineEdit(self.line_tab)
+        self.line_y1_input = QLineEdit(self.line_tab)
+        self.line_x2_input = QLineEdit(self.line_tab)
+        self.line_y2_input = QLineEdit(self.line_tab)
 
-        self.name_input.setGeometry(QtCore.QRect(30, 30, 191, 31))
-        self.point_x_input.setGeometry(QtCore.QRect(80, 80, 111, 31))
-        self.point_y_input.setGeometry(QtCore.QRect(250, 80, 111, 31))
-        self.line_x1_input.setGeometry(QtCore.QRect(70, 70, 111, 31))
-        self.line_y1_input.setGeometry(QtCore.QRect(240, 70, 111, 31))
-        self.line_x2_input.setGeometry(QtCore.QRect(70, 190, 111, 31))
-        self.line_y2_input.setGeometry(QtCore.QRect(240, 190, 111, 31))
+        self.name_input.setGeometry(QRect(30, 30, 191, 31))
+        self.point_x_input.setGeometry(QRect(80, 80, 111, 31))
+        self.point_y_input.setGeometry(QRect(250, 80, 111, 31))
+        self.line_x1_input.setGeometry(QRect(70, 70, 111, 31))
+        self.line_y1_input.setGeometry(QRect(240, 70, 111, 31))
+        self.line_x2_input.setGeometry(QRect(70, 190, 111, 31))
+        self.line_y2_input.setGeometry(QRect(240, 190, 111, 31))
 
         # Input validator
-        validator = QtGui.QDoubleValidator()
+        validator = QDoubleValidator()
         self.point_x_input.setValidator(validator)
         self.point_y_input.setValidator(validator)
         self.line_x1_input.setValidator(validator)
@@ -85,35 +90,49 @@ class InputDialog(QtWidgets.QDialog):
         self.line_x2_input.setValidator(validator)
         self.line_y2_input.setValidator(validator)
 
+        # Wireframe file selector
+        self.text_edit = QTextEdit(self.wireframe_tab)
+        self.open_button = QPushButton("Open CSV File", self.wireframe_tab)
+        self.open_button.clicked.connect(self.open_file)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.text_edit)
+        layout.addWidget(self.open_button)
+        self.wireframe_tab.setLayout(layout)
+
         # Ok and Cancel Buttons
-        self.button_box = QtWidgets.QDialogButtonBox(self)
-        self.button_box.setGeometry(QtCore.QRect(10, 440, 621, 32))
-        self.button_box.setOrientation(QtCore.Qt.Horizontal)
-        self.button_box.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel|QtWidgets.QDialogButtonBox.Ok)
+        self.button_box = QDialogButtonBox(self)
+        self.button_box.setGeometry(QRect(10, 440, 621, 32))
+        self.button_box.setOrientation(Qt.Horizontal)
+        self.button_box.setStandardButtons(QDialogButtonBox.Cancel | QDialogButtonBox.Ok)
         self.button_box.accepted.connect(self.ok_callback)
         self.button_box.rejected.connect(self.cancel_callback)
 
-        QtCore.QMetaObject.connectSlotsByName(self)
+        QMetaObject.connectSlotsByName(self)
 
     def ok_callback(self):
         name = self.name_input.text()
 
         current_index = self.tab_widget.currentIndex()
-        if current_index == 0:
+        if current_index == 0:  # Point
             x = float(self.point_x_input.text())
             y = float(self.point_y_input.text())
-            coordinates = [QtCore.QPointF(x, y)]
+            coordinates = [QPointF(x, y)]
             graphical_object = Point(name, coordinates)
-        elif current_index == 1:
+        elif current_index == 1:  # Line
             x1 = float(self.line_x1_input.text())
             y1 = float(self.line_y1_input.text())
             x2 = float(self.line_x2_input.text())
             y2 = float(self.line_y2_input.text())
-            coordinates = [QtCore.QPointF(x1, y1), QtCore.QPointF(x2, y2)]
+            coordinates = [QPointF(x1, y1), QPointF(x2, y2)]
             graphical_object = Line(name, coordinates)
-        elif current_index == 2:
-            # To do: develop wireframe ...
+        elif current_index == 2:  # Wireframe
             coordinates = []
+            wireframe_data = self.text_edit.toPlainText()
+            lines = wireframe_data.strip().split("\n")
+            for i in range(1, len(lines)):
+                x, y = map(float, lines[i].split(","))
+                coordinates.append(QPointF(x, y))
             graphical_object = Wireframe(name, coordinates)
 
         main_window = self.parent()
@@ -126,3 +145,15 @@ class InputDialog(QtWidgets.QDialog):
 
     def cancel_callback(self):
         super().reject()
+
+    def open_file(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.ReadOnly
+
+        file_name, _ = QFileDialog.getOpenFileName(self, "Open Wireframe File", "./wireframe_files", \
+                                                         "CSV Files (*.csv);;All Files (*)", options=options)
+
+        if file_name:
+            with open(file_name, "r") as file:
+                content = file.read()
+                self.text_edit.setPlainText(content)
